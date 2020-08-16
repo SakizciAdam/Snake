ef55a4516eb48653b4d53bb46b4ebbbb9effedca
SakizciAdam <durulozcan@hotmail.com>
2020-08-16 23:04:47 +0300

    Fixed error

--- src/com/github/sakizciadam/snake/API.java
+++ src/com/github/sakizciadam/snake/API.java
@@ -2,7 +2,7 @@ package com.github.sakizciadam.snake;
 
 import java.awt.*;
 
-public abstract class API {
+public class API {
     private API instance;
     private SnakeGame game;
 
@@ -11,11 +11,17 @@ public abstract class API {
         instance=this;
     }
 
-    public abstract void onRender(final int stage);
+    public void onRender(final int stage){
 
-    public abstract void onPaint(final Graphics g);
+    }
 
-    public abstract void onMove(final int stage,final int[] x,final  int[] y);
+    public void onPaint(final Graphics g){
+
+    }
+
+    public void onMove(final int stage,final int[] x,final  int[] y){
+
+    }
 
     protected API getAPI(){
         return instance;
